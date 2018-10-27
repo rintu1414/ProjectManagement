e80a2fb25f336c419c9e230475a415f4f9a05bc7
Gantayat <email@example.com>
2018-10-26 20:34:19 -0400

    cross origin code

--- src/main/java/com/sra/projectmanagement/controller/UserController.java
+++ src/main/java/com/sra/projectmanagement/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="*", maxAge=3600)
 @RestController
 @RequestMapping("/api")
 public class UserController {
